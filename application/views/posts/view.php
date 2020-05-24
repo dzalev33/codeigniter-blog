e12efa1c9ee4683cfaec3b17cad06e361a6c1344
Dzalev <dzalev@hotmail.com>
2020-05-24 22:32:15 +0200

    comments section

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -1,6 +1,6 @@
 <h2><?php echo $post['title']; ?></h2>
 <small class="post-date">Posted on: <?php echo $post['created_at']; ?></small><br>
-<img class="" src="<?php echo site_url();?>images/posts/<?php echo $post['post_image']; ?>">
+<img class="" src="<?php echo site_url(); ?>images/posts/<?php echo $post['post_image']; ?>">
 <div class="post-body">
 	<?php echo $post['body']; ?>
 </div>
@@ -9,9 +9,38 @@
 
 <a class="btn  btn-dark float-left mr-3" href="edit/<?php echo $post['slug'] ?>">Edit</a>
 
-<?php echo form_open('/posts/delete/'.$post['id']); ?>
+<?php echo form_open('/posts/delete/' . $post['id']); ?>
 
 <input type="submit" value="Delete" class="btn badge-danger">
 </form>
+<hr>
+<h3>Comments</h3>
+<?php if ($comments) : ?>
+	<?php foreach ($comments as $comment) : ?>
+	<div class="well">
+		<h5><?php echo $comment['body']; ?> [by <strong><?php echo $comment['name']; ?></strong>]</h5>
+	</div>
+		<?php endforeach ?>
+<?php else : ?>
+	<p>No comments to display</p>
+<?php endif; ?>
 
-
+<hr>
+<h3>Add Comment</h3>
+<?php echo validation_errors(); ?>
+<?php echo form_open('comments/create/' . $post['id']); ?>
+<div class="form-group">
+	<label>Name</label>
+	<input type="text" name="name" class="form-control">
+</div>
+<div class="form-group">
+	<label>Email</label>
+	<input type="text" name="email" class="form-control">
+</div>
+<div class="form-group">
+	<label>Body</label>
+	<input type="text" name="body" class="form-control">
+</div>
+<input type="hidden" name="slug" value="<?php echo $post['slug'] ?> ">
+<button class="btn badge-primary" type="submit">Submit</button>
+</form>
